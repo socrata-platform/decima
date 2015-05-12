6b1f16fef0b8b956fcf6b21f9a94534f0249c8ed
Michael Brown <michael.brown@socrata.com>
2015-05-12 14:32:34 -0700

    rename start script

--- bin/start_decima.sh
+++ bin/start_decima.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Start Lachesis service
+# Start Decima service
 BASEDIR=$(dirname $0)/..
-CONFIG=${LACHESIS_CONFIG:-$BASEDIR/conf/default.conf}
+CONFIG=${DECIMA_CONFIG:-$BASEDIR/conf/default.conf}
 JARFILE=$BASEDIR/target/scala-2.11/lachesis-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
